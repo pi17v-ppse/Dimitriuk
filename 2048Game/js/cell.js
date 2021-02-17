b2fd56462624294c64bfe46ba3b2928659c09cbb
DimitriukDmitriy <assassinsguysbest@gmail.com>
2021-02-17 21:00:01 +0300

    cell.js some methods added

--- 2048Game/js/cell.js
+++ 2048Game/js/cell.js
@@ -8,7 +8,28 @@ class Cell {
         });
 
         if (Math.random() > 0.8) {
-            this.element.innerHTML = Math.random() > 0.5 ? 4 : 2;
+            this.value = Math.random() > 0.5 ? 4 : 2;
         }
+
+        this.element.onclick = function (e) {
+            this.merge;
+        }.bind(this);
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        this._value = value;
+        this.element.innerHTML = value;
+    }
+
+    clear() {
+        this.value = '';
+    }
+
+    merge() {
+        this.value *= 2;
     }
 }
